0c01ea430b06258430aec367884cbe70c0b4a4ec
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-04-17 13:49:26 +0000

    Removed score update.
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@66 8f21f390-a30d-0410-b651-b82b18e590ef

--- logout.php
+++ logout.php
@@ -10,14 +10,11 @@ if (checkAuth()) {
 			exit();
 		}
 
-		//Update score, and last_request
-		score_func($login_id,0);
-
 		$time_to_set = time() - 1800; //30 mins ago
 		$db->query('UPDATE [game]_users SET last_request = %u WHERE ' .
 		 'login_id = %u', array($time_to_set, $login_id));
 
-		//only logging out to gamelisting
+		// Only logging out to gamelisting
 		if (isset($logout_single_game)) {
 			insert_history($login_id, 'Logged out of ' . $db_name);
 			$db->query('UPDATE user_accounts SET in_game = NULL WHERE ' .
