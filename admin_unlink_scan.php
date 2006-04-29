f5499936b8cc6e4878979ba67945e9a7bddb7eab
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-04-29 18:43:12 +0000

    Cleaned up - less use of db() functions
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@86 8f21f390-a30d-0410-b651-b82b18e590ef

--- admin_unlink_scan.php
+++ admin_unlink_scan.php
@@ -2,36 +2,77 @@
 
 require_once('inc/admin.inc.php');
 
+function map_systems(&$map_array, $current_system)
+{
+	array_push($map_array,$current_system);
+
+	// Get the star info
+	$sInfo = $db->query('SELECT * FROM [game]_stars WHERE star_id = %u', 
+	 array($current_system));
+	$star = $db->fetchRow($sInfo, ROW_ASSOC);
+
+	// Loop through each linked system
+	for ($linknum = 1; $linknum <= 6; ++$linknum) {
+		if ($star['link_' . $linknum] && 
+		     !in_array($star['link_' .$linknum], $map_array)) {
+			map_systems($map_array, $star["link_$linknum"]);
+		}
+	}
+
+}
+
+function add_link($star, $link_to)
+{
+	// Look for an empty link slot
+	for ($linknum = 1; $linknum <= 6; ++$linknum) {
+		if ($star['link_' . $linknum] == $link_to) {
+			return false;
+		}
+		if (empty($star['link_' . $linknum])) {
+			break;
+		}
+	}
+
+	// No available links
+	if($linknum > 6) { 
+		return false;
+	} else {
+		$star['link_' . $linknum] = $link_to;
+		$db->query('UPDATE [game]_stars SET link_%u = %u WHERE star_id = %u',
+		 array($linknum, $link_to, $star['star_id']));
+	}
+}
+
 $map_array = array();
-map_systems($map_array,1); //Crawl through systems from sol and record all systems found.
+map_systems($map_array, 1);
 
 $number_of_islands = 0;
 
-//Find what stars aren't connected to sol that can be linked
-$sql_blank_link = "( (link_1 = 0) OR (link_2 = 0) OR (link_3 = 0) OR (link_4 = 0) OR (link_5 = 0) OR (link_6 = 0) )";
+// Find what stars aren't connected to sol that can be linked
+$stars = $db->query('SELECT * FROM [game]_stars WHERE link_1 = 0 OR ' .
+ 'link_2 = 0 OR link_3 = 0 OR link_4 = 0 OR link_5 = 0 OR link_6 = 0');
 
-db("select * from [game]_stars where $sql_blank_link");
-
-while($star = dbr()) {
-	if(in_array($star['star_id'],$map_array)) { //Skip it if it was found in the crawl
+while ($loneStar = $db->fetchRow($stars, ROW_ASSOC)) {
+	// Skip it if it was found in the crawl
+	if (in_array($loneStar['star_id'], $map_array)) {
 		continue;
 	}
 
 	$island_systems = array();
-	map_systems($island_systems,$star['star_id']);	//Crawl through the island
+	map_systems($island_systems, $loneStar['star_id']); //Crawl through the island
 	++$number_of_islands;
 
-
-	//Go through each star of the island
+	// Go through each star of the island
 	foreach ($island_systems as $island_star) {
 		$sInfo = $db->query("select * from [game]_stars where star_id = $island_star");
 		$star = $db->fetchRow($sInfo);
+
 		//Find the nearest system that has been mapped to sol and link the two
 		$sql_star_distance = "SQRT(POWER(x - $star[x], 2) + POWER(y - $star[y], 2))";
 
-		db2("select *,$sql_star_distance AS distance from [game]_stars where $sql_blank_link order by distance");
+		db2("select *, $sql_star_distance AS distance from [game]_stars where $sql_blank_link order by distance");
 
-		while($nearby_star = dbr2()) {
+		while ($nearby_star = dbr2()) {
 			if(!in_array($nearby_star['star_id'],$map_array)) { //Skip it if it isn't connected to Sol
 				continue;
 			}
@@ -39,13 +80,14 @@ while($star = dbr()) {
 			$star1 = $star;
 			$star2 = $nearby_star;
 
-			//Find the closest star in the island to this star
+			// Find the closest star in the island to this star
 			$sql_star_distance = "SQRT(POWER(x - $star2[x], 2) + POWER(y - $star2[y], 2))";
 
 			db2("select * from [game]_stars where ($sql_star_distance < $star2[distance]) AND $sql_blank_link order by $sql_star_distance");
 
 			while ($closer_star = dbr2()) {
-				if (!in_array($closer_star['star_id'], $island_systems)) { //Skip it if it isn't in the island
+				// Skip it if it isn't in the island
+				if (!in_array($closer_star['star_id'], $island_systems)) {
 					continue;
 				}
 
@@ -62,54 +104,6 @@ while($star = dbr()) {
 	$map_array = array_merge($map_array,$island_systems); //Add the island systems into the list
 }
 
-
-print_page("Unlinked Scan", "$number_of_islands islands found and linked.");
-
-
-
-function map_systems(&$map_array,$current_system)
-{
-	global $db_name;
-
-
-	array_push($map_array,$current_system);
-
-	db2("select * from [game]_stars where star_id = $current_system"); //Get the star info
-	$star = dbr2();
-	$linknum = 1;
-
-	while($linknum <= 6) { //Loop through each linked system
-		if($star["link_$linknum"] && !in_array($star["link_$linknum"],$map_array)) {
-			map_systems($map_array,$star["link_$linknum"]);
-		}
-
-		$linknum++;
-	}
-
-}
-
-function add_link($star,$link_to)
-{
-	global $db_name;
-
-	$linknum = 1;
-
-	while($linknum <= 6) { //Look for an empty link slot
-		if($star["link_$linknum"] == $link_to) {
-			return false;
-		}
-		if(empty($star["link_$linknum"])) {
-			break;
-		}
-		$linknum++;
-	}
-
-	if($linknum > 6) { //No available links
-		return false;
-	} else {
-		$star["link_$linknum"] = $link_to;
-		dbn("update [game]_stars set link_$linknum = $link_to where star_id = $star[star_id]");
-	}
-}
+print_page('Unlinked Scan', "$number_of_islands islands found and linked.");
 
 ?>
