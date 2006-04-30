58d5e541c0131b989c8cc52c50a52e49d1efe3ad
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-04-30 19:08:32 +0000

    New templates
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@102 8f21f390-a30d-0410-b651-b82b18e590ef

---
+++ tpl/generic/game_join.tpl.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('PATH_SAVANT')) exit();
+
+$title = 'Join game';
+
+include('inc/header_splash.tpl.php');
+
+?><h1>Join <?php $this->eprint($this->gameName); ?></h1>
+
+<form action="<?php echo esc(URL_SELF); ?>" method="post">
+	<dl>
+		<dt><label for="in_game_name">Name you would like to play 
+		under</label></dt>
+		<dd><input name="in_game_name" id="in_game_name" value="<?php 
+$this->eprint($this->accountName); ?>" class="text" /></dd>
+
+		<dt><label for="ship_name">Title of your first ship</label></dt>
+		<dd><input name="ship_name" id="ship_name" class="text" /></dd>
+
+		<dt><input type="submit" value="Join" class="button" />
+	<input type="hidden" name="game_selected" value="<?php 
+$this->eprint($this->gameSelected); ?>" /></dt>
+	</dl>
+</form>
+<?php
+
+?><h2>Return to <a href="game_listing.php">game listing</a></h2>
+<?php
+
+include('inc/footer_splash.tpl.php');
+
+?>
---
+++ tpl/generic/game_join_problems.tpl.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('PATH_SAVANT')) exit();
+
+require_once('inc/list.inc.php');
+
+$title = 'Problems joining game';
+
+include('inc/header_splash.tpl.php');
+
+?><h1>Cannot join game</h1>
+<?php
+
+echo makeList($this, 'Problems', $this->problems);
+
+?><h2><a href="<?php $this->eprint($this->returnTo); 
+?>" onclick="history.back(); return false;">Go back</a></h2>
+<?php
+
+include('inc/footer_splash.tpl.php');
+
+?>
