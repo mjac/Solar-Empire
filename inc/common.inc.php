9f34c7ff38169b125c002a77dcf8b15619eeac3e
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-05-19 16:00:06 +0000

    Removed name formatting functions
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@116 8f21f390-a30d-0410-b651-b82b18e590ef

--- inc/common.inc.php
+++ inc/common.inc.php
@@ -316,31 +316,7 @@ function empty_bays(&$ship)
 }
 
 
-function formatName($id, $name, $clanId, $clanSym, $clanCol)
-{
-	static $cache = array();
-
-	if ($name === NULL) {
-		return '<em>Galactic control</em>';
-	}
-
-	if ($id === NULL) {
-		return esc($name);
-	}
-
-	if (!isset($cache[$name])) {
-		$cache[$name] = '<a href="' . esc('player_info.php?target=' . $id) .
-		 '">' . esc($name) . '</a>';
-
-		if ($clanId !== NULL) {
-			$cache[$name] = clanSymbol($clanSym, $clanCol) . " $cache[$name]";
-		}
-	}
-
-	return $cache[$name];
-}
-
-
+/*
 //print clickable name of $player
 function print_name($player)
 {
@@ -361,18 +337,7 @@ function print_name($player)
 	}
 
 	return $cache[$player['login_id']];
-}
-
-function clanSymbol($symbol, $colour)
-{
-	return "<span style=\"color: #" . str_pad(dechex($colour), 6, '0', 
-	 STR_PAD_LEFT) . ";\">" . esc($symbol) . "</span>";
-}
-
-function clanName($name, $symbol, $colour)
-{
-	return esc($name) . ' (' . clanSymbol($symbol, $colour) . ')';
-}
+}*/
 
 
 /*********************
