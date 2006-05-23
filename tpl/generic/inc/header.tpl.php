fcc9d266975cbbe118a7584e8c175f1bcbe48e2f
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-05-23 18:18:39 +0000

    Updated JavaScript location
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@123 8f21f390-a30d-0410-b651-b82b18e590ef

--- tpl/generic/inc/header.tpl.php
+++ tpl/generic/inc/header.tpl.php
@@ -15,6 +15,7 @@ $this->eprint(isset($this->title) ? $this->title :
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link type="text/css" rel="stylesheet" href="<?php 
 $this->eprint($this->url['tpl']); ?>/css/generic.css" />
-<script type="text/javascript" src="js/common.js"></script>
+<script type="text/javascript" src="<?php 
+$this->eprint($this->url['tpl']); ?>/js/common.js"></script>
 </head>
 <body>
