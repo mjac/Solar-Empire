87bde1ea2b2140236f092771b0a73c0e70bf2bbc
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-05-23 20:39:54 +0000

    Contains data instead of schema files
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@131 8f21f390-a30d-0410-b651-b82b18e590ef

---
+++ install/data.inc.php
@@ -0,0 +1,57 @@
+<?php
+
+$dat = array();
+
+$dat['tips'] = array(
+	"To customise your SE experience try playing with the variables on the " .
+	 "<em>options page</em>.",
+	"You can change your colour scheme at any time from the options page.
+There are plenty to choose from.",
+	"Rule Number One: The Admin Is Always Right.<br />
+Rule Number Two: If The Admin Is Wrong, See Rule Number One.<br />
+{starfox25, Dec 06 2000 - 14:26}",
+	"Just because a ship is more expensive does not necassarily mean it is " .
+	 "better.",
+	"The only source of knowledge is experience.<br />
+{Albert Einstein}",
+	"Do not repeat the tactics which have gained you one victory, but let your 
+methods be regulated by the infinite variety of circumstances.<br />
+{Sun Tzu, The Art of War - 6:28, 300BC}",
+	"Nothing is foolproof to a sufficiently talented fool.<br />
+{CrymsonKyng, Apr 21 2001 - 05:56}",
+	"You can click on the Mini-map to get a complete picture of the universe.",
+	"Clicking a player's name gives you information about that player.<br />
+This can also be done with your own name, and will reveal several new options.",
+	"If you find any bugs, report them to the admin, along with details as to 
+what you where doing to get it.",
+	"Autowarp allows you to automatically find your way between A and B.
+It is not necassarily the shortest route though.",
+	"Wormholes offer a great way to get across the universe.",
+	"It's generally possible to get things on the cheap using the auction house.
+As well as lots of things you can't get anywhere else in the game.<br />
+You can get to it from any star-port.",
+	"You should change all your password every few months.<br />
+You should also never give your password to other players. Ever!",
+	"Upgrades allow you to improve your star-ships, however they cannot be 
+removed once installed.",
+	"Joining a clan can get you new friends and allies but also new foes.",
+	"Statistics about the game you are in can be found by clicking on the games 
+name in the top left corner of the screen.",
+	"You may only own one flagship class ship at a time.  If you loose it, 
+the next one will cost double.",
+	"Transversers with the <em>Wormhole Stabiliser</em> upgrade are ideal for 
+getting colonists onto your planets quickly and cheaply.",
+	"The hardest thing of all for a soldier is to retreat.<br />
+{Duke of Wellington}",
+	"Wise people learn when they can; fools learn when they must.<br />
+{Duke of Wellington}",
+	"Never interrupt your enemy when he is making a mistake.<br />
+{Napoleon Bonaparte}",
+	"You must not fight too often with one enemy or you will teach him all " .
+	 "your art of war.<br />
+{Napoleon Bonaparte}",
+	"You should not use one password for all applications; have a different 
+password for each account."
+)
+
+?>
