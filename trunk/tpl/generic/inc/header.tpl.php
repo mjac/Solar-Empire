0378f5847a121e69e5614949567c27193bfd3467
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-06-03 21:16:57 +0000

    Added a new line after the XML definition
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@166 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/tpl/generic/inc/header.tpl.php
+++ trunk/tpl/generic/inc/header.tpl.php
@@ -3,7 +3,7 @@ if (!defined('PATH_SAVANT')) exit();
 
 ob_start('ob_gzhandler');
 
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
