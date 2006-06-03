2803a1da9abc28a3a1108d716a1220c140b84bb8
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-06-03 21:12:10 +0000

    XML header wasn't liked
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@165 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/tpl/generic/inc/header.tpl.php
+++ trunk/tpl/generic/inc/header.tpl.php
@@ -3,8 +3,9 @@ if (!defined('PATH_SAVANT')) exit();
 
 ob_start('ob_gzhandler');
 
-?><?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
 <title><?php
