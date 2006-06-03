131a58fb68611e53d35df8365d28182583e3e287
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-06-03 20:16:29 +0000

    Created a separate section for System Wars
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@161 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/tpl/generic/credits.tpl.php
+++ trunk/tpl/generic/credits.tpl.php
@@ -1,12 +1,21 @@
 <?php
 if (!defined('PATH_SAVANT')) exit();
 
-$title = 'Credits';
+$title = 'Credits, a list of contributors to System Wars';
 
 include('inc/header_splash.tpl.php');
 
-?><h1>Solar Empire Credits</h1>
+?><h1>Credits, contributors to System Wars</h1>
 
+<h2>System Wars</h2>
+<p>Developed by <a href="http://www.mjac.co.uk/">Mjac</a>, originally based on 
+the public domain Generic 2.2.2 release.</p>
+<h3>Other contributors</h3>
+<ul>
+	<li><a href="http://www.imod.ca/">Joshua Eaton</a> (Lex Luthor)</li>
+</ul>
+
+<h2>Generic 2.2.2</h2>
 <dl>
 	<dt>Original designer</dt>
 	<dd><a href="http://www.bryanlivingston.com/">Bryan Livingston</a>
@@ -17,9 +26,7 @@ include('inc/header_splash.tpl.php');
 
 	<dt>Open-source programming</dt>
 	<dd>Moriarty, KilerCris, TheRumour,
-	<a href="http://www.quantum-star.com/">Maugrim_The_Reaper</a>,
-	Semicolon, <a href="http://djc.people.sgalliance.com/">DJCapelis</a> and
-	<a href="http://www.mjac.co.uk/">Mjac</a></dd>
+	<a href="http://www.quantum-star.com/">Maugrim_The_Reaper</a></dd>
 
 	<dt>Renders</dt>
 	<dd>Admiral V'Pier (meshes from
