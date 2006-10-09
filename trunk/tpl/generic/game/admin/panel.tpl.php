84dc2909054cadb60fc5ac2b439fe11694572efa
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-10-09 19:53:35 +0000

    Remove relative links
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@201 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/tpl/generic/game/admin/panel.tpl.php
+++ trunk/tpl/generic/game/admin/panel.tpl.php
@@ -11,13 +11,27 @@ include($this->loadTemplate('game/inc/header_game.tpl.php'));
 
 <h2>Game Functions</h2>
 <ul>
-	<li><a href="admin_edit_vars.php">Edit variables</a></li>
-	<li>Set status to <a href="admin.php?status=hidden">hidden</a>, 
-	<a href="admin.php?status=paused">paused</a> or 
-	<a href="admin.php?status=running">running</a></li>
-	<li><a href="admin.php?reset=1">Reset game</a></li>
-	<li><a href="admin.php?difficulty=1">Change stated difficulty</a></li>
-	<li><form method="post" action="admin.php">
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin_edit_vars.php'); 
+?>">Edit variables</a></li>
+	<li>Set status to <a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?status=hidden'); 
+?>">hidden</a>, 
+	<a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?status=paused'); 
+?>">paused</a> or 
+	<a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?status=running'); 
+?>">running</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?reset=1'); 
+?>">Reset game</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?difficulty=1'); 
+?>">Change stated difficulty</a></li>
+	<li><form method="post" action="<?php
+$this->eprint($this->url['base'] . '/admin.php'); 
+?>">
 		<p><input type="text" name="finishes" value="YYYY-MM-DD HH:MM:SS"
 		 class="text" />
 		<input type="submit" value="Change finish date" class="button" /></p>
@@ -26,32 +40,47 @@ include($this->loadTemplate('game/inc/header_game.tpl.php'));
 
 <h2>Godlike Abilities</h2>
 <ul>
-	<li><a href="admin_build_universe.php?build_universe=1&amp;process=1">Create
-	the universe</a></li>
-	<li><a href="admin.php?preview=1">Preview a universe</a></li>
-	<li><a href="admin_build_universe.php?gen_new_maps=1&amp;process=1">Generate
-	maps</a></li>
-	<li><a href="admin_edit_links.php">Edit star links</a></li>
-	<li><a href="admin_unlink_scan.php">Link star islands</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin_build_universe.php'); 
+?>">Universe generator</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin_edit_links.php'); 
+?>">Edit star links</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin_unlink_scan.php'); 
+?>">Link star islands</a></li>
 </ul>
 
 <h2>Communications</h2>
 <ul>
-	<li><a href="message.php?target=-4">Message everyone</a></li>
-	<li><a href="admin.php?post_game_news=1">Post news</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/message.php?target=-4'); 
+?>">Message everyone</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?post_game_news=1'); 
+?>">Post news</a></li>
 </ul>
 
 <h2>Players</h2>
 <ul>
-	<li><a href="admin_ban_player.php">Ban player</a></li>
-	<li><a href="admin.php?show_active=1">View online players</a></li>
-	<li><a href="admin.php?more_money=1">Give money</a></li>
+	<li><a href="<?php 
+$this->eprint($this->url['base'] . '/admin_ban_player.php'); 
+?>">Ban player</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?show_active=1'); 
+?>">View online players</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?more_money=1'); 
+?>">Give money</a></li>
 </ul>
 
 <h2>General</h2>
 <ul>
-	<li><a href="admin.php?descr=1">Change the game description</a></li>
-	<li><a href="admin.php?messag=1">Change the introduction message</a></li>
+	<li><a href="<?php $this->eprint($this->url['base'] . '/admin.php?descr=1'); 
+?>">Change the game description</a></li>
+	<li><a href="<?php
+$this->eprint($this->url['base'] . '/admin.php?messag=1'); 
+?>">Change the introduction message</a></li>
 </ul>
 <?php
 
