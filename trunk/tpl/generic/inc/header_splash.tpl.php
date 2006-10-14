6d8aaa0a7782252d0b7399ad949370f811549901
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2006-10-14 19:21:58 +0000

    Changed banner
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@225 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/tpl/generic/inc/header_splash.tpl.php
+++ trunk/tpl/generic/inc/header_splash.tpl.php
@@ -4,5 +4,7 @@ class_exists('Savant2') || exit;
 include($this->loadTemplate('inc/header.tpl.php'));
 
 ?><div id="splash">
-<div id="logo"><img src="img/se_logo.jpg" alt="Solar Empire" /></div>
+<div id="logo"><img src="<?php
+$this->eprint($this->url['base'] . '/img/systemwars.jpg');
+?>" alt="Solar Empire: System Wars" /></div>
 
