5460e5295712f1f5336bd444f23c02f6c9d2503a
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2007-01-30 21:17:50 +0000

    Added new benchmarking class to measure periods of time
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@246 8f21f390-a30d-0410-b651-b82b18e590ef

---
+++ trunk/lib/benchmark/benchmark.class.php
@@ -0,0 +1,48 @@
+<?php
+
+/** Measures periods of time */
+class benchmark
+{
+	var $startMS = 0.0;
+	var $startS = 0;
+
+	var $stopMS = 0.0;
+	var $stopS = 0;
+
+	function start()
+	{
+	    $start = explode(' ', microtime());
+	    $this->startMS = (float)$start[0];
+	    $this->startS = (int)$start[1];
+	}
+
+	function stop()
+	{
+	    $stop = explode(' ', microtime());
+	    $this->stopMS = (float)$stop[0];
+	    $this->stopS = (int)$stop[1];
+	}
+
+	function period()
+	{
+	    return (float)($stopS - $startS) + ($stopMS - $startMS);
+	}
+};
+
+
+/** Faster interface for benchmark */
+class autoBenchmark extends benchmark
+{
+	function autoBenchmark()
+	{
+		$this->start();
+	}
+
+	function finish()
+	{
+	    $this->stop();
+	    return $this->period;
+	}
+};
+
+?>
