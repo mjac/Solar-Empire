03214153d02d5c95db035a9b909d56ed86d6f459
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2007-06-26 22:32:34 +0000

    Mistake in database class detection
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@361 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/inc/db.inc.php
+++ trunk/inc/db.inc.php
@@ -1,8 +1,8 @@
 <?php
 defined('PATH_INC') || exit;
 
-if (class_exists('swDatabase') ||
-     @include(PATH_INC . '/swDatabase.class.php')) {
+if (!(class_exists('swDatabase') ||
+     @include(PATH_INC . '/swDatabase.class.php'))) {
 	exit('Cannot find swDatabase class');
 }
 
