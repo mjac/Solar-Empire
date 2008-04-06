f917802197d7bdda33b38faf94ef51a8c85f88f6
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2008-04-06 10:10:26 +0000

    Added my_config section
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@406 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/concept/config.inc.php
+++ trunk/concept/config.inc.php
@@ -1,14 +1,36 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+
+
+
+class my_config_url extends config_url
+{
+
+}
+
+class my_config_path extends config_path
+{
+
+}
+
+class my_config_db extends config_db
+{
+
+}
 
 class config_url
 {
-	public $base = '';
-	public $style = '';
-	public $domain = '';
+	public $base;
+	public $style;
+	public $domain;
+	public $self;
 	
-	public function initiate()
+	public function __construct()
 	{
-		self::$url = configUrl;
+		$this->self = $_SERVER['SCRIPT_NAME'];
+		$this->base = dirname($this->self);
+		$this->domain = $_SERVER['HTTP_HOST'];
+		$this->style = $_SER
 	}
 }
 
@@ -49,11 +71,11 @@ class config
 	{
 	}
 	
-	public function initiate()
+	public static function initiate()
 	{
-		self::$url = new config_url;
-		self::$path = new config_path;
-		self::$db = new config_db;
+		self::$url = new my_config_url;
+		self::$path = new my_config_path;
+		self::$db = new my_config_db;
 	}
 }
 
