ef6efe4ea61ef8d4944d351b5aaa91b1f40d3d6d
mjacdotuk <mjacdotuk@8f21f390-a30d-0410-b651-b82b18e590ef>
2008-04-15 14:15:19 +0000

    Initial account work
    git-svn-id: http://solar-empire.svn.sourceforge.net/svnroot/solar-empire@407 8f21f390-a30d-0410-b651-b82b18e590ef

--- trunk/concept/inc/account.class.php
+++ trunk/concept/inc/account.class.php
@@ -1,8 +1,64 @@
 <?php
 
+/*
+Example database schema for permissions system
+
+Perm_objects
+1 file admin/account.php
+
+Perms
+id type id_from id_to object allow/deny
+
+Load all at once and cache
+
+Global, strong ip bans
+
+game/ matches game/....
+
+account->permission->add(user, file, 'game/', false) // part of the ip ban script
+*/
+
 class account
 {
+	private $id;
 
+	public id()
+	{
+	    return $this->id;
+	}
+
+	public __construct($account_id = false)
+	{
+	    if ($account_id !== false) {
+	        // user
+	        $account_id = 1;//valid;
+	    }
+
+	    $this->id = $account_id;
+	}
+
+	private access_load()
+	{
+	    // load from db
+	}
+
+	public access($page_name, $cache = true)
+	{
+	    // database query that looks up user group
+	}
+};
+
+class account_mutable extends account
+{
+	public __construct($account_id = false)
+	{
+	    parent::construct($account_id);
+	}
+
+	public create()
+	{
+
+	}
 }
 
 ?>
--- trunk/concept/inc/player.class.php
+++ trunk/concept/inc/player.class.php
@@ -4,7 +4,18 @@ require('account.class.php');
 
 class player extends account
 {
+	public __construct($player_id = false)
+	{
+	    parent::construct($player_id);
+	}
+};
 
-}
+class player_mutable extends player
+{
+	public __construct($player_id = false)
+	{
+	    parent::construct($player_id);
+	}
+};
 
 ?>
